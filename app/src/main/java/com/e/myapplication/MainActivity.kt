89b49876ba45ab93088858380d02d98bcd97e4cd
linahandayani <lina.handayani06@gmail.com>
2020-09-12 07:22:14 +0100

    succes

--- app/src/main/java/com/e/myapplication/MainActivity.kt
+++ app/src/main/java/com/e/myapplication/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)s
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val networkConnection = NetworkConnection(applicationContext)
